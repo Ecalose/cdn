0823d761b7e542eb50b0aff7203fc90e0640c331
Ecalose <47114714+Ecalose@users.noreply.github.com>
2021-02-23 23:22:08 +0800

    Update update-repo.sh

--- update-repo.sh
+++ update-repo.sh
@@ -4,5 +4,6 @@ status_log=$(git status -sb)
 if [ "$status_log" == "## main...origin/main" ];then
   echo "nothing to commit, working tree clean"
 else
-  git add .&&git commit -m "update by github actions"&&git push origin main
+#  git add .&&git commit -m "update by github actions"&&git push origin main
+  git add .&&git commit -m "update by github actions"
 fi
